abaf858245244cc64997b9257de053cf11c54acb
Prakash R <prakashramanukolanu1998@gmail.com>
2023-08-22 18:16:21 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# Get all todos
+
+# Get a single todo
+
+# Create a todo
+
+# Update a todo
+
+# Delete a todo
